8ea6bfa610ff5eb9cf0ed5cc11add255a8d55fb0
TaufikM3 <341.mth@gmail.com>
2024-07-09 11:19:09 +0700

    perbaikan pada action dan store.js juga style

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -1,9 +1,9 @@
-import vue from "vue";
-import vuex from "vuex";
-import persistedState from "vuex-persistedstate";
-import actions from "./actions";
-import mutations from "./mutations";
-import state from "./state";
+import vue from 'vue';
+import vuex from 'vuex';
+import persistedState from 'vuex-persistedstate';
+import actions from './actions';
+import mutations from './mutations';
+import state from './state';
 vue.use(vuex);
 
 export const store = new vuex.Store({
